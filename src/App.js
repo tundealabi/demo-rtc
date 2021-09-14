07f5a3f5e7262ac0c894b833765ed957dfab8e2f
Tunde Alabi <tundealabi4780@gmail.com>
2021-09-14 02:29:42 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
+import CentrifugeClient from 'centrifuge';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const centrifugeConnect = new CentrifugeClient('wss://realtime.zuri.chat/connection/websocket');
+  useEffect(() => {
+    centrifugeConnect.on('connect', function (ctx) {
+      console.log('connected', ctx);
+    });
+
+    centrifugeConnect.on('disconnect', function (ctx) {
+      console.log('disconnected', ctx);
+    });
+
+    centrifugeConnect.subscribe('goalstest', function (ctx) {
+      console.log('goalstest', ctx);
+    });
+    centrifugeConnect.subscribe('edit_vision', function (ctx) {
+      console.log('edit_vision', ctx);
+    });
+
+    centrifugeConnect.connect();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
